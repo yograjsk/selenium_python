8f2c86a63a357f98525e7ba773625358d3e4f374
yograjsk <yograjsk@gmail.com>
2019-11-07 08:05:29 +0530

    implicite and explicite wait conditions

--- OrangeHRM/checkWelcome.py
+++ OrangeHRM/checkWelcome.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 import time
 
 class CheckWelcome:
-
+    '''
+    implicite wait = applicatble for all elements
+    explicite wait = perticular / desired element
+    '''
 
     def __init__(self):
         self.username = "Admin"
@@ -15,8 +22,9 @@ class CheckWelcome:
         # url = 'https://opensource-demo.orangehrmlive.com/index.php/auth/login'
         # location = '../drivers/'
         self.driver = webdriver.Chrome(executable_path=self.location + 'chromedriver.exe')
+        # self.driver.implicitly_wait(5)
         self.driver.get(self.url)
-        time.sleep(2)
+        # time.sleep(2)
 
     def login(self):
         self.driver.find_element_by_name('txtUsername').send_keys('Admin')
@@ -24,13 +32,20 @@ class CheckWelcome:
         self.driver.find_element_by_name('Submit').click()
 
     def checkWelcome(self):
+
         welcomeText = self.driver.find_element_by_id('welcome').text
         print(welcomeText)
         assert "Welcome "+self.username in welcomeText
 
     def logout(self):
-        self.driver.find_element_by_id("welcome").click()
-        time.sleep(2)
+        # time.sleep(2)
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable(self.driver.find_element_by_id("welcome")))
+        element.click()
+        # wait.until(EC.element_to_be_clickable(self.driver.find_element(By.ID, "welcome"))).click()
+        # self.driver.find_element_by_id("welcome").click()
+        # self.driver.find_element(By.ID, "welcome").click()
+        # time.sleep(2)
         self.driver.find_element_by_link_text("Logout").click()
 
 check1 = CheckWelcome()
